e74ce55c53d57f57345a30dcca9fae8a685c4aa4
naskhi <nursandyihksan97@gmail.com>
2024-12-21 01:39:50 +0700

    add CRUD StockOut

---
+++ app/Filament/Resources/StockOutResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StockOutResource\Pages;
+use App\Filament\Resources\StockOutResource\RelationManagers;
+use App\Models\StockOut;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StockOutResource extends Resource
+{
+    protected static ?string $model = StockOut::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('kode_barang')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('no_barang_keluar')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('quantity')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('destination')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('tanggal_keluar')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('ID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('kode_barang')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('no_barang_keluar')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('destination')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tanggal_keluar')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStockOuts::route('/'),
+            'create' => Pages\CreateStockOut::route('/create'),
+            'edit' => Pages\EditStockOut::route('/{record}/edit'),
+        ];
+    }
+}
